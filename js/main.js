8c201e37dc7577b4cd4b519c180d077a5f1ba333
Filip Wasiewski <fiodor.wasiewski@gmail.com>
2021-07-05 16:03:05 +0200

    version4

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ document.getElementsByClassName('mobile-close')[0].addEventListener('click', fun
 document.getElementById('appointment-form').addEventListener('submit', function(e) {
     e.preventDefault();
 
-    const appointmentMessage = document.querySelector('.appointment-message');
+    const appointmentMessage = document.querySelector('#appointment-message');
     let formFields = document.getElementsByClassName('form-field');
     let allFields = false;
     let appointment = {
@@ -33,7 +33,7 @@ document.getElementById('appointment-form').addEventListener('submit', function(
     };
 
     if(allFields) {
-        console.log('wysłanie');
+        console.log(appointment);
     } else {
         appointmentMessage.classList.add('error');
         appointmentMessage.innerText = `wypełnij pole`;
